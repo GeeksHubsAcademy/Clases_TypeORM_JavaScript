8b40786b197552aeccd352bff7067730c2c74753
Xavi <xavi@geekshubs.com>
2020-04-27 17:15:20 +0200

    Creando nuestra primera conexión

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
-
+import { createConnection } from 'typeorm';
 
 const app = express();
 app.use(express.json);
+
+createConnection()
+.then(async connection=>{
+    console.log("Conectado");
+}).catch(error=>console.error(error));
 
 
 app.listen(3000, () => {
