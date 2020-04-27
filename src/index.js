2bc28739828b7b6553c4039ba6e8be76d5b8afea
Xavi <xavi@geekshubs.com>
2020-04-27 18:31:48 +0200

    Trabajando con nuestro repositorios realizando búsquedas

--- src/index.js
+++ src/index.js
@@ -9,13 +9,34 @@ createConnection()
 .then(async connection=>{
     console.log("Conectado");
     const user = new User();
-    user.firstName="Xavi";
-    user.lastName="Rodríguez";
-    user.isActive=true;
+    user.firstName="Juan";
+    user.lastName="Pérez";
+    user.isActive=false;
+
+    const userRepository = await connection.getRepository("user");
+    //Obtenemos todos los registros.
+    const users= await userRepository.find();
+    console.log("Uusarios" , users);
+    //Obtenemos un registro podemos indicar us id.
+    const userOne = await userRepository.findOne(6);
+    console.log("Un unico usuario" , userOne);
+    //Obtenemos los registros que cumpla unas condiciones.
+    const usersActive = await userRepository.find({isActive:false});
+    console.log("Usuarios inactivos",usersActive);
+    //Obtener todos los registros y el número total.
+    const [allUsers, numberUsers] = await userRepository.findAndCount();
+    console.log("Usuarios:",allUsers);
+    console.log("Número total de usuarios", numberUsers);
+    //Obtener usuarios ordenados por id desc
+    const usersOrderById = await userRepository.find({order:{id:"DESC"}});
+    console.log("Usuarios ordenados de forma descednete", usersOrderById);
+
+
     connection.manager
         .save(user)
         .then(user=>console.log("Usuario creado"+user.id))
         .catch(error=>console.error(error));
+    
 }).catch(error=>console.error(error));
 
 app.listen(3000, () => {
