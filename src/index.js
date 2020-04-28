242e186c09d1af1e0e2e5c68b00432b97172cb6d
Xavi <xavi@geekshubs.com>
2020-04-28 13:19:03 +0200

    mEjemplo de transacción

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import express from 'express';
-import { getConnectionManager, createConnection, getCustomRepository } from 'typeorm';
+import { getConnection, createConnection, getCustomRepository, InsertQueryBuilder } from 'typeorm';
 import { User } from './entities/User';
 import { UserRepository } from './repositories/userRepository';
 
 const app = express();
 app.use(express.json);
 
+const transcationFunction = async()=>{
 
+    const connection = await createConnection();
+    const queryRunner = connection.createQueryRunner();
+
+    //Estableciendo la conexión
+    await queryRunner.connect();
+
+    //Iniciando la transacción.
+
+    await queryRunner.startTransaction();
+  
+    try{
+        const user= new User();
+        user.firstName="Rafa";
+        user.lastName="Rodriguez";
+        user.isActive = true;
+        //Confirmando la conexión.
+        await queryRunner.manager.save(user);
+         await queryRunner.commitTransaction();
+         console.log("Guardado usuario");
+    }catch(err){
+        //Tirando hacía atrás la conexión.
+        await queryRunner.rollbackTransaction();
+        console.error(err);
+    }finally{
+        //Cerrando la conexión.
+        await queryRunner.release();
+    }
+
+
+
+
+}
+
+transcationFunction();
+
+
+
+
+
+/*
 const repo = async ()=>{
 const connection = await createConnection();
 const userRepository = new getCustomRepository(UserRepository);
@@ -20,6 +61,7 @@ console.log("Eliminado", await userRepository.deleteUser(1));
 }
 
 repo();
+*/
 
 app.listen(3000, () => {
     console.log('Ready on port 3000!');
